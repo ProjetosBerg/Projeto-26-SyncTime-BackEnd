990f4afdb55ce1a52726c46030587ecc6ec4c08b
Bergkley <bergkley@gmail.com>
2026-02-17 10:22:54 -0300

    feat: projeto SyncTime BackEnd

--- server.ts
+++ server.ts
@@ -4,13 +4,13 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import env from "env-var";
-import router from "./routes";
+import router from "./src/routes";
 import databaseHelper from "@/loaders/database";
 import logger from "@/loaders/logger";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "@/config/swagger";
 import cors from "cors";
-import { initSocket, getIoInstance } from "./lib/socket";
+import { initSocket, getIoInstance } from "./src/lib/socket";
 
 const app = express();
 const server = http.createServer(app);
